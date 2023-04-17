dbf7fbd7114876a2bc0225a5f993516d2fe9f9b7
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2023-04-17 13:39:43 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,115 +1,78 @@
-import React, {useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import {
-  StyleSheet, Text, View, TextInput,
-  Keyboard, // импорт компонента клавиатуры
-  KeyboardAvoidingView, // Фикс перекрытия
-  TouchableWithoutFeedback, // импорт компонента обертки
-  Platform,
-  Alert,
-  Button, 
-} from 'react-native';
+import React, {useCallback, useState, useEffect} from 'react';
+import {} from 'react-native';
 import * as Font from 'expo-font';
 import { useFonts } from 'expo-font';
-import { AppLoading } from 'expo';
 
-const loadFonts = async () => {
-  await Font.useFonts({
-    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
-    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
-  });
-};
+import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
 
-// const [loadAsync] = useFonts{(
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-// )} 
+import LoginScreen from './Screens/LoginScreen';
+import RegistrationScr from './Screens/RegistrationScreen';
+import Home from './Screens/Home';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  // const [value, setValue] = useState("");
-  // const inputHandler = (text) => setValue(text);
-
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-
-  const nameHandler = (text) => setName(text);
-  const passwordHandler = (text) => setPassword(text);
-
-  const onLogin = () => {
-    Alert.alert("Credentials", `${name} + ${password}`);
-  };
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      ...Platform.select({
-        ios: {
-          backgroundColor: '#000000'
-        },
-        android: {
-          backgroundColor: '#ffffff'
-        },
-      }),
-    },
-  });
+  const [isReady, setIsReady] = useState(false);
   
-  // чтобы наши шрифты загрузились до момента появления приложения на экране
-  const [isReady, setIsReady] = useState(false) 
-  if (!isReady) {
-    return <AppLoading
-      startAsync={loadFonts}
-      onFinish={() => setIsReady(true)}
-      onError={console.warn} />
+
+  // useEffect(() => {
+  //   const onChange = () => {
+  //     const width = Dimensions.get('window').width;
+  //     console.log('width', width);
+  //   };
+  //   Dimensions.addEventListener('change', onChange);
+  //   // return () => {
+  //   //   // Dimensions.removeEventListener('change', onChange);
+  //   // }
+  // },[])
+
+  const loadFonts = async () => {
+    await FontFace.loadAsync({
+      'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    });
   };
+  
+  const [fontsLoaded] = useFonts({
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Bold': require("./assets/fonts/Roboto-Bold.ttf"),
+    'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
+  })
+  
+  const onLayoutRootView = useCallback(async () => {
+  if (fontsLoaded) {
+    await SplashScreen.hideAsync();
+  }
+  }, [fontsLoaded]);
+  
+  if (!fontsLoaded) {
+    return null;
+  };
+
+     // чтобы наши шрифты загрузились до момента появления приложения на экране
+  if (!isReady) {
+    return (<AppLoading
+      startAsync={fontsLoaded}
+      onFinish={() => setIsReady(true)}
+      onError={console.warn}
+    />);
+  };
+
+  const MainStack = createStackNavigator(); // указывает на группу навигаторов
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-        <KeyboardAvoidingView // определяем ОС и настраиваем поведение клавиатуры
-          behavior={Platform.OS == "ios" ? "padding" : "height"}
-        >
-          <TextInput
-            value={name}
-            onChangeText={nameHandler}
-            placeholder='Username'
-            style={styles.input}
-          />
-          <TextInput
-            value={password}
-            onChangeText={passwordHandler}
-            placeholder='Password'
-            secureTextEntry={true}
-            style={styles.input}
-          />
-          <Button title={'Login'} style={styles.input} onPress={onLogin} />
-          {/* <TextInput
-              placeholder="Type text"
-              value={value}
-              onChangeText={inputHandler}
-            /> */}
-          </KeyboardAvoidingView>
-        </View>
-      </TouchableWithoutFeedback>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ecf0f1',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingBottom: 30,
-  },
-  input: {
-    width: 200,
-    height: 44,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: 'black',
-    marginBottom: 10,
-    fontFamily:"Roboto-Regular",
-  },
-});
+    <NavigationContainer>
+      <MainStack.Navigator initialRouteName='Login'>
+        {/* Замена Switch */}
+        <MainStack.Screen name='Registration' component={RegistrationScr} />{/* Замена Route */}
+        <MainStack.Screen name='Login' component={LoginScreen} />
+        <MainStack.Screen name='Home' component={Home} options={{title: 'Start screeni'}} />
+      </MainStack.Navigator>
+    </NavigationContainer>
+    );
+  
+};
 
